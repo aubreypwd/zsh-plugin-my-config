2df98bf355d6f9ebf948b4e43720e4d95525ca2d
Aubrey Portwood <code@aubreypwd.com>
2022-07-03 19:33:22 -0600

    Quietly do these.
    These can come back with errors when too many are run at the same time.

--- my-require.zsh
+++ my-require.zsh
@@ -17,46 +17,49 @@ if [[ $(pwd) == "$HOME" ]]; then
 		echo "  Please install: https://github.com/aubreypwd/zsh-plugin-require"
 	else
 
-		###
-		 # Install homebrew itself.
-		 #
-		 # You probably have this installed aready though.
-		 #
-		 # E.g: brew
-		 #
-		 # @since Friday, 10/2/2020
-		 # @see   https://brew.sh
-		 ##
-		require "brew" '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"'
+		() {
 
-		###
-		 # Required packages, and how to install them.
-		 #
-		 # @since Friday, 10/2/2020
-		 ##
-		require "cmatrix" "brew reinstall cmatrix" "brew" # Easy matrix screensave.
-		require "composer" "brew reinstall composer" "brew" # Composer, @see https://composer.org
-		require "exa" "brew reinstall exa" "brew" # Exa makes ls even more awesome.
-		require "ffmpeg" "brew reinstall ffmpeg" "brew"
-		require "fzf" "brew reinstall fzf" "brew"
-		require "ghq" "brew reinstall ghq" "brew" # required by git get alias.
-		require "git" "brew reinstall git" "brew"
-		require "highlight" "brew reinstall highlight" "brew" # Highlighting cat
-		require "http" "brew install httpie" "brew" # Better than curl, replaces curl.
-		require "m" "brew install m-cli" "brew" # Love m-cli!
-		require "mycli" "brew reinstall mycli" "brew" # Better than mysql
-		require "n" "brew reinstall n" "brew" # n auto, n lts, etc
-		require "npm" "brew reinstall node" "brew" # Also installs node.
-		require "python" "brew reinstall python" "brew" # Installs pip3 and easy_install
-		require "ruby" "brew reinstall ruby" "brew" # Installs gems
-		require "svn" "brew reinstall subversion" "brew"
-		require "terminal-notifier" "brew reinstall terminal-notifier" "brew" # Terminal notifications
-		require "ttab" "npm install ttab -g" "npm" # Used to open new windows in iTerm, etc.
-		require "vcsh" "brew reinstall vcsh" "brew"
-		require "watch" "brew reinstall watch" "brew"
-		require "watchexec" "brew reinstall watchexec" "brew"
-		require "wget" "brew reinstall wget" "brew"
-		require "wp" "brew reinstall wp-cli" "brew"
+			###
+			 # Install homebrew itself.
+			 #
+			 # You probably have this installed aready though.
+			 #
+			 # E.g: brew
+			 #
+			 # @since Friday, 10/2/2020
+			 # @see   https://brew.sh
+			 ##
+			require "brew" '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"'
+
+			###
+			 # Required packages, and how to install them.
+			 #
+			 # @since Friday, 10/2/2020
+			 ##
+			require "cmatrix" "brew reinstall cmatrix" "brew" # Easy matrix screensave.
+			require "composer" "brew reinstall composer" "brew" # Composer, @see https://composer.org
+			require "exa" "brew reinstall exa" "brew" # Exa makes ls even more awesome.
+			require "ffmpeg" "brew reinstall ffmpeg" "brew"
+			require "fzf" "brew reinstall fzf" "brew"
+			require "ghq" "brew reinstall ghq" "brew" # required by git get alias.
+			require "git" "brew reinstall git" "brew"
+			require "highlight" "brew reinstall highlight" "brew" # Highlighting cat
+			require "http" "brew install httpie" "brew" # Better than curl, replaces curl.
+			require "m" "brew install m-cli" "brew" # Love m-cli!
+			require "mycli" "brew reinstall mycli" "brew" # Better than mysql
+			require "n" "brew reinstall n" "brew" # n auto, n lts, etc
+			require "npm" "brew reinstall node" "brew" # Also installs node.
+			require "python" "brew reinstall python" "brew" # Installs pip3 and easy_install
+			require "ruby" "brew reinstall ruby" "brew" # Installs gems
+			require "svn" "brew reinstall subversion" "brew"
+			require "terminal-notifier" "brew reinstall terminal-notifier" "brew" # Terminal notifications
+			require "ttab" "npm install ttab -g" "npm" # Used to open new windows in iTerm, etc.
+			require "vcsh" "brew reinstall vcsh" "brew"
+			require "watch" "brew reinstall watch" "brew"
+			require "watchexec" "brew reinstall watchexec" "brew"
+			require "wget" "brew reinstall wget" "brew"
+			require "wp" "brew reinstall wp-cli" "brew"
+		} &> /dev/null &!
 	fi
 
 fi
