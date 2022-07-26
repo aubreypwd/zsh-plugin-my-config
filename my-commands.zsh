ae1b2b1be78dbc4497d0d55107a700e224ed11d6
Aubrey Portwood <code@aubreypwd.com>
2022-07-26 14:38:58 -0600

    Remove double ()

--- my-commands.zsh
+++ my-commands.zsh
@@ -219,7 +219,7 @@ nwd() {
  #
  # @since Thursday, July 7, 2022
  ##
-sysinfo()() {
+sysinfo() {
 
 	echo "\e[35mƤ PHP:\e[0m   \e[37m$(phpv)\e[0m" # Show the current working directory.
 	echo "\e[32m⊔ Node:\e[0m  \e[37m$(node --version)\e[0m" # Show the current working directory.
