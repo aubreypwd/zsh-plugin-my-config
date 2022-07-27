bcd415c01e5408d70f46980f603a42375183a66b
Aubrey Portwood <code@aubreypwd.com>
2022-07-27 11:18:04 -0600

    Add useinstead

--- my-deprecations.zsh
+++ my-deprecations.zsh
@@ -1,9 +1,20 @@
-#!/bin/zsh
+#!/bin/bash
+
+###
+ # Instruct someone to use a different command.
+ #
+ # @since Wednesday, July 27, 2022
+ ##
+useinstead() {
+
+	echo "Use [$1] instead, this command has been deprecated."
+	return 1
+}
+
 
 ###
  # Aliases
  ##
-
 alias valet@7="echo 'From within a site directory, use [valet isolate php@7.x] instead.' && (exit 3)"
 alias valet@8="echo 'From within a site directory, use [valet isolate php@8.x] instead.' && (exit 3)"
 alias valetv="echo 'No alternative.' && (exit 33)" # A way to see what version valet is running at.
@@ -12,13 +23,5 @@ alias valets="echo 'localtunnel is unreliable, use [valet share] instead' && (ex
 ###
  # Functions
  ##
-
-###
- # Valet use
- #
- # @since Wednesday, July 6, 2022
- # @since	Friday, July 22, 2022 Deprecated
- ##
-function vu {
-	echo 'Use [valet isolate php@x.x] intead.' && (exit 3);
-}
+vu() { useinstead "valet isolate php@x.x"; }
+watchf() { useinstead "watchfext"; }
