d3f107d011e4d4693e152fae8c8a47194cc7e08d
Aubrey Portwood <code@aubreypwd.com>
2022-12-21 11:01:06 -0700

    Add specific file for PHP stuff.

---
+++ my-php.zsh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+###
+ # PHP Configuration, etc
+ #
+ # @since Dec 21, 2022
+ ##
+
+# Make sure all the PHP versions have config files symlinked.
+( (
+
+	php_versions=(7.3 7.4 8.0 8.1 8.2)
+	conf_files=( 'php.ini' 'xdebug-3.ini' )
+
+	for version in "${php_versions[@]}"; do
+		for conf_file in "${conf_files[@]}"; do
+			ln -sf "$HOME/Sites/conf.d/$conf_file" "/opt/homebrew/etc/php/$version/conf.d/z-$conf_file"
+		done
+	done
+
+) 1>&- 2>&- & )
