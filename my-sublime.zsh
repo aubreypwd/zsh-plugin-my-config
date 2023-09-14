c7845afda781f051d3ea5a9db34b746513005e15
Aubrey Portwood <aubreypwd@icloud.com>
2023-09-14 11:42:31 -0600

    Don't set subl this way, am using OpenIn 4 ATM.

--- my-sublime.zsh
+++ my-sublime.zsh
@@ -1,16 +1,17 @@
-#!/bin/zsh
+#!/bin/sh
 
 ###
  # Sublime configurations.
  #
  # @since Jan 16, 2023
+ # @since Sep 14, 2023 No longer setting sublime to the default app, using OpenIn 4.
  ##
 
 ( ( # Quietly run in the background...
 
 	# Keep sublime associated with these extensions.
-	for ext in php js json yml md sh zsh html; do
-		duti -s "osascript -e 'id of app \"Sublime Text\"'" ".$ext" all
-	done
+	# for ext in php js json yml md sh zsh html; do
+		# duti -s "osascript -e 'id of app \"Sublime Text\"'" ".$ext" all
+	# done
 
 ) 1>&- 2>&- & )
