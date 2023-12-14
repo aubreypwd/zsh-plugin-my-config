910f0d7c5ccfefa348de9cde16f2062720f334f0
Aubrey Portwood <aubreypwd@users.noreply.github.com>
2023-12-14 16:10:05 -0700

    Also configure PHP 8.3.

--- my-php.zsh
+++ my-php.zsh
@@ -6,10 +6,9 @@
  # @since Dec 21, 2022
  ##
 
-# Make sure all the PHP versions have config files symlinked.
-( (
+configphp () {
 
-	php_versions=(7.3 7.4 8.0 8.1 8.2)
+	php_versions=(7.3 7.4 8.0 8.1 8.2 8.3)
 	conf_files=( 'php.ini' 'xdebug-3.ini' ) # In ~/.config/php/conf.d/
 
 	for version in "${php_versions[@]}"; do
@@ -17,5 +16,9 @@
 			ln -sf "$HOME/.config/php/conf.d/$conf_file" "/opt/homebrew/etc/php/$version/conf.d/z-$conf_file"
 		done
 	done
+}
 
+# Make sure all the PHP versions have config files symlinked.
+( (
+	configphp
 ) 1>&- 2>&- & )
