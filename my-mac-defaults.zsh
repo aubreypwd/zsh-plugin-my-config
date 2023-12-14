84189e632f91d11c6235ac8e665b7ebeb950ec2e
Aubrey Portwood <aubreypwd@users.noreply.github.com>
2023-12-14 16:09:50 -0700

    Correct screenshots directory.

--- my-mac-defaults.zsh
+++ my-mac-defaults.zsh
@@ -23,7 +23,7 @@
 	defaults write com.apple.TextEdit SmartQuotes -bool false
 	defaults write com.apple.TextEdit SmartDashes -bool false
 	defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
-	defaults write com.apple.screencapture location "$HOME/Pictures/Screenshots"
+	defaults write com.apple.screencapture location "$HOME/Pictures/Screenshots/Autosaved"
 	defaults write com.apple.desktopservices DSDontWriteNetworkStores true
 	defaults write com.apple.Finder QuitMenuItem 1 # Add quit to Finder
 	defaults write com.apple.dock springboard-columns -int 7 # Launchpad Grid
