5dcf98feebd95215bf19ff269f0dd577944e09b1
Aubrey Portwood <aubreypwd@users.noreply.github.com>
2024-06-19 11:30:19 -0600

    Using OpenIn4 right now.

--- my-exts.zsh
+++ my-exts.zsh
@@ -4,6 +4,7 @@
  # My File Extension Associations
  #
  # @since Jul 14, 2023
+ # @since Jun 12, 2024 Disabled because using OpenIn instead.
  ##
 startup-exts () {
 
@@ -11,19 +12,19 @@ startup-exts () {
 	#    osascript -e 'id of app "Finder"'
 
 	# VSCode: com.microsoft.VSCode
-	duti -s com.microsoft.VSCode .php all
-	duti -s com.microsoft.VSCode .js all
-	duti -s com.microsoft.VSCode .jsx all
-	duti -s com.microsoft.VSCode .html all
-	duti -s com.microsoft.VSCode .css all
-	duti -s com.microsoft.VSCode .scss all
-	duti -s com.microsoft.VSCode .sass all
-	duti -s com.microsoft.VSCode .tsx all
-	duti -s com.microsoft.VSCode .sublime-project all
-	duti -s com.microsoft.VSCode .sublime-workspace all
-	duti -s com.microsoft.VSCode .json all
+	# duti -s com.microsoft.VSCode .php all
+	# duti -s com.microsoft.VSCode .js all
+	# duti -s com.microsoft.VSCode .jsx all
+	# duti -s com.microsoft.VSCode .html all
+	# duti -s com.microsoft.VSCode .css all
+	# duti -s com.microsoft.VSCode .scss all
+	# duti -s com.microsoft.VSCode .sass all
+	# duti -s com.microsoft.VSCode .tsx all
+	# duti -s com.microsoft.VSCode .sublime-project all
+	# duti -s com.microsoft.VSCode .sublime-workspace all
+	# duti -s com.microsoft.VSCode .json all
 	
-	# Built-in Terminal
-	duti -s com.apple.Terminal .sh all
-	duti -s com.apple.Terminal .zsh all
+	# # Built-in Terminal
+	# duti -s com.apple.Terminal .sh all
+	# duti -s com.apple.Terminal .zsh all
 }
